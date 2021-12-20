37728cfc0d2070931342a6ffdfebacabb7d039fa
Motolani-D <motolanid@gmail.com>
2021-12-20 14:09:11 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let express = require("express");
+let app = express();
+let path = require("path"); //required when i want to include html page
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public")); //this is how I am accessing static resource e.g. CSS stylesheet
+const router = express.Router();
+
+router.use(function (req, res, next) {
+  let today = new Date().getDay();
+  if (today === 0 || today === 6) return "we arre closed for the week";
+  next();
+});
+
+app.get("/", (req, res) => {
+  console.log(__dirname);
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+app.get("/contact", (req, res) => {
+  console.log(req);
+  res.sendFile(path.join(__dirname, "contact.html"));
+});
+
+app.get("/about", (req, res) => {
+  console.log(req);
+  res.sendFile(path.join(__dirname, "about.html"));
+});
+app.listen(3000, () => {
+  console.log("started running");
+});
